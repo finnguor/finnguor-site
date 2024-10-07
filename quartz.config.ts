425afe0f03b888f38338d3b91d9b843e8942cb78
finnguor <finnguo28@gmail.com>
2024-10-07 11:20:22 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "how are you doing ?",
+    pageTitle: "finnguor: how are you doing ?",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
